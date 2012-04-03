d98bf68775dc5be654f8aaa78c962b89bc50e302
natsumesou <natsumesouxx@gmail.com>
2012-04-03 23:40:46 +0900

    change directory structure

--- popup.js
+++
@@ -1,8 +0,0 @@
-function register(){
-    var form = document.forms.form;
-    var username = form.elements.username.value;
-    var password = form.elements.password.value;
-    console.log(username);
-    localStorage['username'] = username;
-    localStorage['password'] = password;
-}
